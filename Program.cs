bf884ac68bb50ad86b0b475cb87f4d42669f8aed
HoLyDreaM <dreghot@gmail.com>
2024-02-26 11:10:44 +0300

    dosyalar gönderiliyor

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPKAyiklama
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string strZipFile = @"D:\\Sistem\\Users\\Mehmet\\Desktop\\Bot\\Yeni klasör";
+
+            string extractPath = Console.ReadLine();
+            extractPath = Path.GetFullPath(extractPath);
+
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                extractPath += Path.DirectorySeparatorChar;
+
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Gets the full path to ensure that relative segments are removed.
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+
+                        // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                        // are case-insensitive.
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+            }
+        }
+    }
+}
